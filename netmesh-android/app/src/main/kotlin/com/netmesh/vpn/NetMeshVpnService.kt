6fb0874398f2ab3a4261a0001c5276d7193efa7d
tarun225601-star <tarun225601@gmail.com>
2026-07-17 17:52:20 +0530

    Update NetMeshVpnService.kt

--- netmesh-android/app/src/main/kotlin/com/netmesh/vpn/NetMeshVpnService.kt
+++ netmesh-android/app/src/main/kotlin/com/netmesh/vpn/NetMeshVpnService.kt
@@ -1,33 +1,20 @@
-package com.netmesh.vpn
-
-import android.content.Intent
-import android.content.pm.ServiceInfo
-import android.net.VpnService
-import android.os.Build
-
-class NetMeshVpnService : VpnService() {
-
-    override fun onCreate() {
-        super.onCreate()
-        NotificationHelper.createChannel(this)
+override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+    if (intent?.action == "TOGGLE_VPN") {
+        // अगर सर्विस पहले से चल रही है तो बंद करें
+        stopForeground(true)
+        stopSelf()
+        return START_NOT_STICKY
     }
-
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        val notification = NotificationHelper.buildNotification(this, "Active")
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-            startForeground(
-                NotificationHelper.NOTIFICATION_ID,
-                notification,
-                ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE
-            )
-        } else {
-            startForeground(NotificationHelper.NOTIFICATION_ID, notification)
-        }
-
-        // यहाँ आपका मौजूदा VPN स्टार्ट करने का लॉजिक आएगा 
-        // (जैसे TunPacketForwarder को शुरू करना)
-        
-        return START_STICKY
+    
+    val notification = NotificationHelper.buildNotification(this, "Active")
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+        startForeground(
+            NotificationHelper.NOTIFICATION_ID,
+            notification,
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE
+        )
+    } else {
+        startForeground(NotificationHelper.NOTIFICATION_ID, notification)
     }
+    return START_STICKY
 }
