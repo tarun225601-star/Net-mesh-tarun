6d419318d74fd8c19d2542c65ed1c2fc8c2e2d02
tarun225601-star <tarun225601@gmail.com>
2026-07-18 11:54:25 +0530

    Update NetMeshVpnService.kt

--- netmesh-android/app/src/main/kotlin/com/netmesh/vpn/NetMeshVpnService.kt
+++ netmesh-android/app/src/main/kotlin/com/netmesh/vpn/NetMeshVpnService.kt
@@ -1,36 +1,21 @@
-override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
- package com.netmesh.vpn
 package com.netmesh.vpn
 
 import android.app.Service
 import android.content.Intent
-import android.content.pm.ServiceInfo
 import android.net.VpnService
 import android.os.Build
 import android.os.IBinder
 
-import android.app.Service
-import android.content.Intent
-import android.content.pm.ServiceInfo
-import android.net.VpnService
-import android.os.Build
-import android.os.IBinder
-   if (intent?.action == "TOGGLE_VPN") {
-        // अगर सर्विस पहले से चल रही है तो बंद करें
-        stopForeground(true)
-        stopSelf()
-        return START_NOT_STICKY
+class NetMeshVpnService : VpnService() {
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (intent?.action == "TOGGLE_VPN") {
+            stopForeground(true)
+            stopSelf()
+            return START_NOT_STICKY
+        }
+        return START_STICKY
     }
-    
-    val notification = NotificationHelper.buildNotification(this, "Active")
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-        startForeground(
-            NotificationHelper.NOTIFICATION_ID,
-            notification,
-            ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE
-        )
-    } else {
-        startForeground(NotificationHelper.NOTIFICATION_ID, notification)
-    }
-    return START_STICKY
+
+    override fun onBind(intent: Intent?): IBinder? = null
 }
