6630da934583b4d75985e5614c1097eedde9c9d5
tarun225601-star <tarun225601@gmail.com>
2026-07-21 01:23:06 +0530

    Update NetMeshVpnService.kt

--- netmesh-android/app/src/main/kotlin/com/netmesh/vpn/NetMeshVpnService.kt
+++ netmesh-android/app/src/main/kotlin/com/netmesh/vpn/NetMeshVpnService.kt
@@ -1,16 +1,22 @@
+package com.netmesh.vpn
+
+import android.content.Intent
+import android.net.VpnService
+import android.os.ParcelFileDescriptor
+
 class NetMeshVpnService : VpnService() {
     private var vpnInterface: ParcelFileDescriptor? = null
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        // यहाँ आपका tunnel logic शुरू होगा
         val builder = Builder()
-        builder.setSession("NetMeshVPN")
+        
+        // VPN कॉन्फ़िगरेशन
+        vpnInterface = builder.setSession("NetMeshVPN")
             .addAddress("10.0.0.2", 24)
             .addDnsServer("8.8.8.8")
-            .addRoute("0.0.0.0", 0) // यह पूरे ट्रैफिक को टनल के अंदर ले जाएगा
-            .setMtu(1500)
-        
-        vpnInterface = builder.establish()
+            .addRoute("0.0.0.0", 0)
+            .establish()
+            
         return START_STICKY
     }
 
