2b0d64e68dfe49e75c4fc4abfb0e622f814cd779
tarun225601-star <tarun225601@gmail.com>
2026-07-21 19:15:00 +0530

    Update NetMeshVpnService.kt

--- netmesh-android/app/src/main/kotlin/com/netmesh/vpn/NetMeshVpnService.kt
+++ netmesh-android/app/src/main/kotlin/com/netmesh/vpn/NetMeshVpnService.kt
@@ -6,10 +6,7 @@ import android.os.ParcelFileDescriptor
 import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.io.IOException
-import java.net.InetSocketAddress
 import java.nio.ByteBuffer
-import java.nio.channels.DatagramChannel
-import java.nio.channels.SocketChannel
 
 class NetMeshVpnService : VpnService() {
 
@@ -30,14 +27,10 @@ class NetMeshVpnService : VpnService() {
             try {
                 val builder = Builder().apply {
                     setSession("NetMeshTunnel")
-                    // वीपीएन का वर्चुअल आईपी और सबनेट
                     addAddress("10.0.0.2", 24)
-                    // सभी इंटरनेट ट्रैफिक को टनल की तरफ मोड़ना
                     addRoute("0.0.0.0", 0)
-                    // पब्लिक डीएनएस ताकि यूट्यूब और वेबसाइट्स तुरंत नाम पहचान सकें
                     addDnsServer("8.8.8.8")
                     addDnsServer("8.8.4.4")
-                    setConfigureIntent(null)
                 }
 
                 vpnInterface = builder.establish()
@@ -50,14 +43,9 @@ class NetMeshVpnService : VpnService() {
                     while (isRunning) {
                         val length = inputStream.read(packet.array())
                         if (length > 0) {
-                            // यहाँ पैकेट रीड हो रहा है और हम इसे प्रोटेक्टेड सॉकेट से आगे पास करेंगे
                             packet.limit(length)
-                            
-                            // वीपीएन लूप से बचने के लिए पैकेट को वापस उसी आउटपुट स्ट्रीम पर रिफ्लेक्ट कर रहे हैं
-                            // ताकि बिना किसी रुकावट के कनेक्टिविटी बनी रहे
                             outputStream.write(packet.array(), 0, length)
                             outputStream.flush()
-                            
                             packet.clear()
                         } else {
                             Thread.sleep(10)
